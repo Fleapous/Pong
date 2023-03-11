54cd74e3a8c6fab4b5063bfa97224f738ea7fef9
egecan giray <girayegecan02@gmail.com>
2023-03-11 20:35:12 +0100

    Ball and Paddle created

--- Pong/Pong.cpp
+++ Pong/Pong.cpp
@@ -8,9 +8,18 @@ HINSTANCE hInst;
 WCHAR szTitle[MAX_LOADSTRING];
 WCHAR szWindowClass[MAX_LOADSTRING];
 
-ATOM MyRegisterClass(HINSTANCE hInstance);
+WCHAR BallClassName[MAX_LOADSTRING];
+
+ATOM RegisterClassMain(HINSTANCE hInstance);
+ATOM RegisterClassBall(HINSTANCE hInstance);
+ATOM RegisterClassPaddle(HINSTANCE hInstance);
+
 BOOL InitInstance(HINSTANCE, int);
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+LRESULT CALLBACK WndProcMain(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK WndProcBall(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK WndProcPaddle(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
 INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
 
 
@@ -34,10 +43,15 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	std::wstring className(L" TUTORIAL \0");
 	className.copy(szWindowClass, className.size());
 
+	std::wstring ballName(L" ballClassName \0");
+	className.copy(BallClassName, className.size());
+
 	//LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
 	//LoadString(hInstance, IDC_TUTORIAL, szWindowClass, MAX_LOADSTRING);
 
-	MyRegisterClass(hInstance);
+	RegisterClassMain(hInstance);
+	RegisterClassBall(hInstance);
+	RegisterClassPaddle(hInstance);
 
 	if (!InitInstance(hInstance, nCmdShow))
 	{
@@ -59,14 +73,14 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	return static_cast <int>(msg.wParam);
 }
 
-ATOM MyRegisterClass(HINSTANCE hInstance)
+ATOM RegisterClassMain(HINSTANCE hInstance)
 {
 
 	WNDCLASSEXW wcex;
 	wcex.cbSize = sizeof(WNDCLASSEX);
 
-	wcex.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
-	wcex.lpfnWndProc = WndProc;
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProcMain;
 	wcex.cbClsExtra = 0;
 	wcex.cbWndExtra = 0;
 	wcex.hInstance = hInstance;
@@ -84,53 +98,103 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 	return RegisterClassExW(&wcex);
 }
 
+ATOM RegisterClassBall(HINSTANCE hInstance) 
+{
+	WNDCLASSEXW wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProcBall;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = nullptr;
+	wcex.hCursor = nullptr;
+	wcex.hbrBackground = (HBRUSH)CreateSolidBrush(RGB(255, 0, 0));
+	wcex.lpszMenuName = nullptr;
+	wcex.lpszClassName = L"BallClass";
+	wcex.hIconSm = nullptr;
+
+	return RegisterClassExW(&wcex);
+}
+
+ATOM RegisterClassPaddle(HINSTANCE hInstance)
+{
+	WNDCLASSEXW wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProcPaddle;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = nullptr;
+	wcex.hCursor = nullptr;
+	wcex.hbrBackground = (HBRUSH)CreateSolidBrush(RGB(0, 0, 255));
+	wcex.lpszMenuName = nullptr;
+	wcex.lpszClassName = L"PaddleClass";
+	wcex.hIconSm = nullptr;
+
+	return RegisterClassExW(&wcex);
+}
+
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
 	hInst = hInstance;
 	bool tmp;
 
-	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_EX_LAYERED | WS_OVERLAPPED,
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_EX_LAYERED | WS_OVERLAPPED | WS_EX_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance,
 		nullptr);
-	tmp = SetLayeredWindowAttributes(hWnd, 0, (255 * 0.8), LWA_ALPHA);
 
-	//if (!tmp){
-	//	return false;
-	//}
+	SetWindowLong(hWnd, GWL_EXSTYLE, GetWindowLong(hWnd, GWL_EXSTYLE) | WS_EX_LAYERED);
+	SetLayeredWindowAttributes(hWnd, 0, (255 * 0.8), LWA_ALPHA);
 
 	if (!hWnd) {
 		return FALSE;
 	}
 
+	HWND hWndBall = CreateWindowW(L"BallClass", L"BallWindow", WS_CHILD | WS_VISIBLE, 0, 0, 30, 30, hWnd, nullptr, hInstance, nullptr);
+
+	if (!hWndBall)
+	{
+		return FALSE;
+	}
+	
+	//SIZE size;
+	//size.cx = 30;
+	//size.cy = 30;
+
+	//HDC hdc = GetDC(hWndBall);
+	//Ellipse(hdc, 30, 30, 30, 30);
+
+	//POINT point;
+	//point.x = 0;
+	//point.y = 0;
+	//UpdateLayeredWindow(hWndBall, NULL, NULL, &size, hdc, &point, RGB(0, 0, 0), 0, ULW_OPAQUE);
+
+	HWND hWndPaddle = CreateWindowW(L"PaddleClass", L"PaddleWindow", WS_CHILD | WS_VISIBLE, 40, 0, 30, 30, hWnd, nullptr, hInstance, nullptr);
+
+	if (!hWndPaddle)
+	{
+		return FALSE;
+	}
+
 	ShowWindow(hWnd, nCmdShow);
+	ShowWindow(hWndBall, nCmdShow);
+	ShowWindow(hWndPaddle, nCmdShow);
+
 	UpdateWindow(hWnd);
+	UpdateWindow(hWndBall);
+	UpdateWindow(hWndPaddle);
 	return TRUE;
 }
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProcMain(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 
 	switch (message)
 	{
-	//case WM_COMMAND:
-	//{
-	//	int wmId = LOWORD(wParam);
-	//	switch (wmId)
-	//	{
-
-	//		//case IDM_ABOUT:
-	//		//	DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-	//		//	break;
-	//		//case IDM_EXIT:
-	//		//	DestroyWindow(hWnd);
-	//		//	break;
-	//	default:
-	//		return DefWindowProc(hWnd, message, wParam, lParam);
-
-	//	}
-	//}
-	//break;
-
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
@@ -152,12 +216,34 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			clientWidth, clientHeight);
 		SetWindowText(hWnd, s);
 	}
-	//case WM_CREATE:
-	//{
-	//	const COLORREF rgbRed = 0x000000FF;
-	//	SetLayeredWindowAttributes(hWnd, rgbRed, 100, LWA_ALPHA);
-	//}
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
+
+LRESULT CALLBACK WndProcBall(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
+{
+	switch (message) 
+	{
+	case WM_CREATE:
+	{
+		HRGN reg = CreateEllipticRgn(0, 0, 30, 30);
+		SetWindowRgn(hWnd, reg, true);
+	}
 	break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
+
+LRESULT CALLBACK WndProcPaddle(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
 	default:
 		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
